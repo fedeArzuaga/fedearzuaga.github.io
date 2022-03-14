dae4a6a74f3c0cf8469a450b2ac184f31bed1771
Federico Arzuaga <federico.arzuaga@wundermanthompson.com>
2022-03-14 00:18:31 -0300

    Adding some less code

---
+++ src/js/cursor.js
@@ -0,0 +1,30 @@
+const cursor = document.querySelector("#cursor"),
+      pointer = document.querySelector("#pointer"),
+      dataMouseElements = Array.from(document.querySelectorAll('[data-mouse="hover"]'));
+
+document.addEventListener( 'mousemove', e => {
+    cursor.classList.add('on-screen');
+    pointer.classList.add('on-screen');
+
+    cursor.style.left = `${e.clientX}px`;
+    cursor.style.top = `${e.clientY}px`;
+    pointer.style.left = `${e.clientX}px`;
+    pointer.style.top = `${e.clientY}px`;
+});
+
+document.addEventListener( 'mouseout', () => {
+    cursor.classList.remove('on-screen');
+    pointer.classList.remove('on-screen');
+});
+
+for ( let i = 0; i < dataMouseElements.length; i++ ) {
+    dataMouseElements[i].addEventListener( 'mousemove', e => {
+        cursor.classList.add('hover-effect');
+        pointer.classList.add('hover-effect');
+    });
+    dataMouseElements[i].addEventListener( 'mouseout', e => {
+        cursor.classList.remove('hover-effect');
+        pointer.classList.remove('hover-effect');
+    })
+}
+
